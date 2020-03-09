6bdbcb601910303ffee04cdfb7136d59ed0476f9
ricardo <ricardoluliwei@gmail.com>
2020-03-09 15:25:54 -0700

    Initial Commit

---
+++ Proj5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proj5
+//
+//  Created by Ricardo on 3/9/20.
+//  Copyright © 2020 Ricardo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
