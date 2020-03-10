eb6cf80a79854c3d3b4c6f1ecaeac9bd7f6c12a6
ricardo <ricardoluliwei@gmail.com>
2020-03-09 20:44:29 -0700

    finished pq

--- Proj5/MyPriorityQueue.cpp
+++ Proj5/MyPriorityQueue.cpp
@@ -10,7 +10,7 @@
 
 template<typename Object>
 MyPriorityQueue<Object>::MyPriorityQueue(){
-    minHeap.push_back(new Object);
+    minHeap.push_back(Object());
 }
 
 
@@ -51,7 +51,6 @@ const Object & MyPriorityQueue<Object>::min() const
 {
     if (isEmpty()){
         throw PriorityQueueEmptyException("Priority Queue is empty!");
-        return NULL;
     }
     
     return minHeap[1];
@@ -64,8 +63,8 @@ void MyPriorityQueue<Object>::extractMin()
 {
     if (isEmpty()){
            throw PriorityQueueEmptyException("Priority Queue is empty!");
-           return;
     }
+    
     minHeap[1] = minHeap[size()];
     minHeap.pop_back();
     sink(1);
@@ -73,18 +72,91 @@ void MyPriorityQueue<Object>::extractMin()
 
 
 template<typename Object>
+void MyPriorityQueue<Object>::swap(int i, int j)
+{
+    Object temp = minHeap[i];
+        minHeap[i] = minHeap[j];
+        minHeap[j] = temp;
+        return;
+    
+}
+
+template<typename Object>
 void MyPriorityQueue<Object>::sink(int index){
     if (index * 2 > size())
         return;
-    
-    int smaller = minHeap[index * 2];
-    
+
+    int smaller = index * 2;
+
     if (index * 2 + 1 <= size() )
         smaller = minHeap[index * 2] < minHeap[index * 2 + 1] ? index * 2 : index * 2 + 1;
-    
-    
+
+
     if (minHeap[index] > minHeap[smaller]) {
         swap(index, smaller);
         return sink(smaller);
     }
+    
+}
+
+
+//template<typename Object>
+//void MyPriorityQueue<Object>::sink(int index)
+//
+//{
+//    int i = index;
+//    while (i*2 <= size()) {
+//        if ((minHeap[i] < minHeap[i*2]) and (minHeap[i] < minHeap[i*2+1]))
+//            //current node smaller than both right and left
+//        {
+//            break;
+//        }
+//        else if ((minHeap[i] < minHeap[i*2]) and (minHeap[i] > minHeap[i*2+1]))
+//            //current node smaller than left but larger than right
+//        {
+//            swap(i, i*2);
+//            i = i*2;
+//        }
+//        else if ((minHeap[i] > minHeap[i*2]) and (minHeap[i] < minHeap[i*2+1]))
+//            //current node smaller than right but larger than left
+//        {
+//            swap(i, i*2+1);
+//            i = i*2+1;
+//        }
+//        else
+//            //current node larger than both right and left
+//        {
+//            if ((minHeap[i*2] > minHeap[i*2+1]))
+//                //current node's left child larger than right child
+//            {
+//                swap(i, i*2+1);
+//                i = i*2+1;
+//            }
+//            else
+//                //current node's left child smaller than right child
+//            {
+//                swap(i, i*2);
+//                i = i*2;
+//
+//            }
+//        }
+//    }
+//
+//
+//}
+
+template<typename Object>
+void MyPriorityQueue<Object>::floating(int index)
+{
+    if (size() == 1) {
+        return;
+    }
+    for (int i = index; i > 1; i /= 2)
+        {
+            if (minHeap[i] < minHeap[i/2])
+                swap(i, i/2);
+            else
+                break;
+        }
+
 }
--- Proj5/main.cpp
+++ Proj5/main.cpp
@@ -1,24 +1,36 @@
 #include "proj5.hpp"
 #include "edge.hpp"
-#include "MyPriorityQueue.hpp"
+#include "MyPriorityQueue.cpp"
 #include <iostream>
+#include <random>
 
 using namespace std;
 
-void testSink(){
+void testPriorityQueue(){
     MyPriorityQueue<int> a;
-    a.insert(1);
-    a.insert(2);
-    a.insert(3);
-    a.insert(4);
-    a.insert(5);
-    a.insert(6);
-    a.extractMin();
-    cout << a.min() << endl;
+    for (int i = 0; i < 10000; i++) {
+        a.insert(rand() % 5000);
+    }
+    
+    int max = -1;
+    
+    for (int i = 0; i < 10000; i++) {
+        int head = a.min();
+        if (head < max) {
+            cout << "Error" << endl;
+            return;
+        }
+        max = head;
+        a.extractMin();
+    }
+    
+    cout << a.isEmpty() << endl;
+    cout << (a.size() == 0) << endl;
+    cout << "Pass test" << endl;
 }
 
 int main()
 {
-    testSink();
+    testPriorityQueue();
 	return 0;
 }
