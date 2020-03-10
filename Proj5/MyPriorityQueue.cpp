55903fb3d6af6ebb555426c5ffabcbbf85c973a6
Kaining Mao <chrissmao99@gmail.com>
2020-03-09 19:32:16 -0700

    Complete swap and float but not sink

--- Proj5/MyPriorityQueue.cpp
+++ Proj5/MyPriorityQueue.cpp
@@ -9,6 +9,66 @@
 #include "MyPriorityQueue.hpp"
 
 template<typename Object>
+void MyPriorityQueue<Object>::swap(int i, int j)
+{
+    Object temp = minHeap[i];
+        minHeap[i] = minHeap[j];
+        minHeap[j] = temp;
+        return;
+    
+}
+
+template<typename Object>
+void MyPriorityQueue<Object>::sink(int index)
+
+{
+    int i = index;
+    while (i*2 < size()) {
+        if ((minHeap[i] < minHeap[i*2]) and (minHeap[i] < minHeap[i*2+1]))
+            //current node smaller than both right and left
+        {
+            break;
+        }
+        else if ((minHeap[i] < minHeap[i*2]) and (minHeap[i] > minHeap[i*2+1]))
+            //current node smaller than left but larger than right
+        {
+            swap(i, i*2);
+            i = i*2;
+        }
+        else if ((minHeap[i] > minHeap[i*2]) and (minHeap[i] < minHeap[i*2+1]))
+            //current node smaller than right but larger than left
+        {
+            swap(i, i*2+1);
+            i = i*2+1;
+        }
+        else
+        {
+            if (<#condition#>) {
+                <#statements#>
+            }
+        }
+    }
+
+
+}
+
+template<typename Object>
+void MyPriorityQueue<Object>::floating(int index)
+{
+    if (size() == 1) {
+        return;
+    }
+    for (int i = index; i > 0; i /= 2)
+        {
+            if (minHeap[i] < minHeap[i/2])
+                swap(i, i/2);
+            else
+                break;
+        }
+
+}
+
+template<typename Object>
 size_t MyPriorityQueue<Object>::size() const noexcept
 {
     return minHeap.size() - 1; // stub
--- Proj5/main.cpp
+++ Proj5/main.cpp
@@ -1,7 +1,16 @@
 #include "proj5.hpp"
+#include <vector>
+#include "iostream"
 
 
 int main()
 {
-	return 0;
+
+    int index = 9;
+    for (int i = index; i > 0; i /= 2)
+        {
+            std::cout<<i<<std::endl;
+        }
+    
+    std::cout<<1/2<<std::endl;
 }
